f9568215e4e6a397b35ce5e39b54da63cc85309c
xuzhiwei <sq1725696495@sina.com>
2021-06-03 16:32:37 +0800

    dawd

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,38 +1,20 @@
 //Page Object
 Page({
   data: {
-    
+    //轮播图数组
+    siwperList:[]
   },
+  //页面开始加载就会触发
   //options(Object)
   onLoad: function(options){
-    
+    //发送异步请求获取轮播图数据    优化手段   promise
+    wx.request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+      success: (result)=>{
+        this.setData({
+          siwperList:result.data.message
+        })
+      },
+    });
   },
-  onReady: function(){
-    
-  },
-  onShow: function(){
-    
-  },
-  onHide: function(){
-
-  },
-  onUnload: function(){
-
-  },
-  onPullDownRefresh: function(){
-
-  },
-  onReachBottom: function(){
-
-  },
-  onShareAppMessage: function(){
-
-  },
-  onPageScroll: function(){
-
-  },
-  //item(index,pagePath,text)
-  onTabItemTap:function(item){
-
-  }
 });
