624a9ec17d433e76eb2459417a321988b8b72f77
xuzhiwei <sq1725696495@sina.com>
2021-06-09 17:41:48 +0800

    asd

--- pages/category/index.js
+++ pages/category/index.js
@@ -7,8 +7,13 @@ Page({
    * 页面的初始数据
    */
   data: {
+    //左侧的菜单数据
     leftMenuList:[],
+    //右侧的商品数据
+    rightContent:[],
   },
+  //接口返回数据
+  Cates:[],
 
   onLoad:function(options){
     this.getCates();
@@ -19,7 +24,16 @@ Page({
       url:"https://api-hmugo-web.itheima.net/api/public/v1/categories"
     })
     .then(res =>{
-      console.log(res);
+      this.Cates=res.data.message;
+      //构造左侧的大菜单数据
+      let leftMenuList = this.Cates.map(v=>v.cat_name);
+      //构造右侧的商品数据
+      let rightContent = this.Cates[0].children;
+
+      this.setData({
+        leftMenuList,
+        rightContent
+      })
     })
   }
 })
